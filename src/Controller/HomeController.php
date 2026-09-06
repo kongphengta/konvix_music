bca5d5c877e8e88acb36b302cca9e40b10355f94
kongphengta <kong.vixay@gmail.com>
2026-09-06 11:20:07 +0200

    Add home pagination with 12 songs per page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,18 +2,25 @@
 
 namespace App\Controller;
 
+use App\Repository\SongRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use App\Repository\SongRepository;
-
 
 final class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(SongRepository $songRepository): Response
+    public function index(SongRepository $songRepository, Request $request): Response
     {
-        $latestSongs = $songRepository->findBy([], ['id' => 'DESC'], 6);
+        $page = max(1, $request->query->getInt('page', 1));
+        $limit = 12;
+        $totalSongs = $songRepository->count([]);
+        $totalPages = max(1, (int) ceil($totalSongs / $limit));
+        $page = min($page, $totalPages);
+        $offset = ($page - 1) * $limit;
+
+        $latestSongs = $songRepository->findBy([], ['id' => 'DESC'], $limit, $offset);
 
         $latestSongs = array_values(array_filter(
             $latestSongs,
@@ -22,6 +29,9 @@ final class HomeController extends AbstractController
 
         return $this->render('home/index.html.twig', [
             'latestSongs' => $latestSongs,
+            'currentPage' => $page,
+            'totalPages' => $totalPages,
+            'totalSongs' => $totalSongs,
         ]);
     }
 }
